8851d35d9410c1948e571a4bf054f7d92b04e217
mom0tomo <momo76w@gmail.com>
2018-09-22 14:26:51 +0900

    create User controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :commits, dependent: :destroy
   has_many :repositories, dependent: :destroy
 
-  validates :uid, presence: true, length: { maximum: 50 }
+  validates :uid, presence: true, uniqueness: true, length: { maximum: 50 }
 
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users, only: [:show, :new, :create]
 end
